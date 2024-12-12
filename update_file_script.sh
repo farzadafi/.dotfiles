8fb703ba996baec083f4495ec5b4481228460e3f
farzadafi <farzadafi40@gmail.com>
2024-12-12 21:55:43 +0330

    add some code for update files

---
+++ update_file_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# array for source and target file pairs :)
+declare -A file_pairs=(
+    ['/home/farzad/.atamanrc.config']='/home/farzad/.dotfiles/ataman/.atamanrc.config'
+    #["/path/to/source/file1.txt"]="/path/to/repo/tracked_file1.txt"
+)
+
+for SOURCE_FILE in "${!file_pairs[@]}"; do
+    TARGET_FILE="${file_pairs[$SOURCE_FILE]}"
+
+    if [ -f "$SOURCE_FILE" ]; then
+        cp "$SOURCE_FILE" "$TARGET_FILE"
+        echo "Updated $TARGET_FILE with content from $SOURCE_FILE."
+
+        git add "$TARGET_FILE"
+        echo "$TARGET_FILE staged for commit."
+    else
+        echo "Source file $SOURCE_FILE does not exist."
+    fi
+done
+
+echo "All files processed."
+
